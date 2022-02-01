2a5fbd5ed34e7c1a375a43cc95a5f93996b82cd5
Alex <tuluc.alexandru@gmail.com>
2022-02-01 20:31:27 +0200

    Added initial viewport calculation in JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+
+// We execute the same script as before
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
